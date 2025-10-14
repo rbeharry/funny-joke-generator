f5052c83aee7323b078e5a11d7c4118f827cfb13
rbeharry <rlbeharry@hotmail.com>
2025-10-14 17:31:55 -0400

    Added corrected code to JS and HTML

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-
-function generateJoke(event){
+import axios from "axios";
+import Typewriter from "typewriter-effect/dist/core";
+function displayJoke(response) {
+  new Typewriter("#typewriter-text", {
+      strings: response.data.answer,
+      autoStart: true,
+    });
+  function generateJoke(event) {
     event.preventDefault();
+
     
-    let jokeFormElement = document.querySelector("#joke-generator-form");
-jokeFormElement.addEventListener("submit", generateJoke);
-}
 
+    let instructionsInput = document.querySelector("#joke-generator-form");
+    let apiKey = "3fbc16ffbdt190o64c90aa576419eec5";
+    let prompt = `Generate a joke from user ${instructionsInput.value}`;
+    let context =
+      "You are a very funny comedian. Please tell me something funny";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    axios.get(apiURL).then(displayJoke);
+  }
 
-new Typewriter("#joke", {
-  strings: "Let me tell you something funny!",
-  autoStart: true,
-  delay: 1,
-  cursor: "",
-});
-
-
-   
-
-
-
-
+  let jokeformElement = document.querySelector("#generate-joke-button");
+  jokeformElement.addEventListener("click", generateJoke);
