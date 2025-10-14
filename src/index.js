549638a7c03b6a7664f57a996a73ea4bf1140bd8
rbeharry <rlbeharry@hotmail.com>
2025-10-14 17:38:49 -0400

    Updated code in js and html

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import axios from "axios";
-import Typewriter from "typewriter-effect/dist/core";
 function displayJoke(response) {
-  new Typewriter("#typewriter-text", {
-      strings: response.data.answer,
-      autoStart: true,
-    });
-  function generateJoke(event) {
-    event.preventDefault();
+  new Typewriter("#joke", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+function tellFunnyJoke(event) {
+  event.preventDefault();
 
-    
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "3fbc16ffbdt190o64c90aa576419eec5";
+  let context = "You are a comedian who tells very funny jokes";
+  let prompt = `Generate a very funny joke about ${userInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    let instructionsInput = document.querySelector("#joke-generator-form");
-    let apiKey = "3fbc16ffbdt190o64c90aa576419eec5";
-    let prompt = `Generate a joke from user ${instructionsInput.value}`;
-    let context =
-      "You are a very funny comedian. Please tell me something funny";
-    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-    axios.get(apiURL).then(displayJoke);
-  }
+  axios.get(apiURL).then(displayJoke);
+}
 
-  let jokeformElement = document.querySelector("#generate-joke-button");
-  jokeformElement.addEventListener("click", generateJoke);
+let generatorButton = document.querySelector("#generator-joke-button");
+generatorButton.classList.remove("hidden");
+generatorButton.addEventListener("click", tellFunnyJoke);
+
