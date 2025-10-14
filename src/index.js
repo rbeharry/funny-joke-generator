1ab71c32f123f5a81540db07d0974eb3abb31811
rbeharry <rlbeharry@hotmail.com>
2025-10-13 21:38:26 -0400

    Added typewriter effect to js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
+
 function generateJoke(event){
     event.preventDefault();
-    let jokeElement=document.querySelector("#joke");
-
-new Typewriter("#typewriter", {
-strings: "Let me tell you something funny",
-autoStart: true,
-delay: 1,
-cursor: "",
- 
-});
-   
-new Typewriter("#typewriter", {
-  strings: "Front-end developer + communications specialist",
-  autoStart: true,
- 
-});
-
-  
-   
-
-
-let jokeFormElement = document.querySelector("#joke-generator-form");
+    
+    let jokeFormElement = document.querySelector("#joke-generator-form");
 jokeFormElement.addEventListener("submit", generateJoke);
 }
+
+
+new Typewriter("#joke", {
+  strings: "Let me tell you something funny!",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+
+
+   
+
+
+
+
